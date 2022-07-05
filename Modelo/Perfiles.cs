2224f1a79969a2c0b4cb4b8a1eb21f5e62393519
tomas arias karle <tomas.arias2001@gmail.com>
2022-07-05 09:45:03 -0300

    agregar singleton/controladora y modelo

--- Modelo/Perfiles.cs
+++ Modelo/Perfiles.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Controladora
+namespace Modelo
 {
-    public class Class1
+    internal class Perfiles
     {
     }
 }
