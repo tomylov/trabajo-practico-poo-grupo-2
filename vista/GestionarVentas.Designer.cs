e4bd2be7a12281c3da8e2ee412772a39b92cfdc7
tomas arias <tomas.arias2001@gmail.com>
2022-07-27 18:03:46 -0300

    interfaces datagrid/vista

--- vista/GestionarVentas.Designer.cs
+++ vista/GestionarVentas.Designer.cs
@@ -37,9 +37,11 @@ namespace vista
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(41, 32);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(673, 273);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
--- vista/GestionarVentas.cs
+++ vista/GestionarVentas.cs
@@ -20,7 +20,42 @@ namespace vista
             dataGridView1.DataSource = Controladora.sql_consulta.Ejecutar(CMD).Tables[0];
             btnvta.Enabled = false;
             btnestado.Enabled = false;
+            datagridinterfaz();
+
         }
+
+        public void datagridinterfaz()
+        {
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.White;
+            dataGridView1.BackgroundColor = Color.Gray;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.Green;
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.Black;
+            dataGridView1.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.Beige;
+            dataGridView1.ColumnHeadersDefaultCellStyle.SelectionForeColor = Color.Green;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font(dataGridView1.Font, FontStyle.Bold);
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Oxygen", 12);
+            dataGridView1.ColumnHeadersHeight = 40;
+            dataGridView1.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.RowHeadersVisible = false;
+        }
+
+
 
         private void btnvta_Click(object sender, EventArgs e)
         {
@@ -33,7 +68,7 @@ namespace vista
         {
             fila = e.RowIndex;
             int valor = dataGridView1.Rows.Count;
-            if (fila == -1 || fila == valor - 1)
+            if (fila == -1 || fila == valor)
             {
                 btnvta.Enabled = false;
                 btnestado.Enabled = false;
